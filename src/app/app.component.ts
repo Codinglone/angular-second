aea66bda9141bf29bf2ff55a94f2ae8c3d3f1c53
codinglone <codinglone@gmail.com>
2023-06-21 20:00:26 +0400

    Shared data from child component to parent component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { PostComponent } from './post/post.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'angular-second';
-  parentMessage: string = 'Message changed!'
+  parentMessage: string = 'Message changed!';
+  message: string = ''
+
+  @ViewChild(PostComponent) childComp!: PostComponent;
+
+  constructor(){
+    console.log(this.childComp);
+    
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.childComp);
+    this.message = this.childComp.childMessage;
+  }
 }
